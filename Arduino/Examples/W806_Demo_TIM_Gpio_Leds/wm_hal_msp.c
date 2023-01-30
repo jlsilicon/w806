35aec7c53b05632b3c06d5039eb4674c11c06ef1
Long John Silicon <52719931+jlsilicon@users.noreply.github.com>
2023-01-30 08:22:27 -0500

    Add files via upload

---
+++ Arduino/Examples/W806_Demo_TIM_Gpio_Leds/wm_hal_msp.c
@@ -0,0 +1,21 @@
+#include "wm_hal.h"
+
+void HAL_MspInit(void)
+{
+
+}
+
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
+{
+    __HAL_RCC_TIM_CLK_ENABLE();
+    HAL_NVIC_SetPriority(TIM_IRQn, 0);
+    HAL_NVIC_EnableIRQ(TIM_IRQn);
+}
+
+void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
+{
+    // TIM，TIM，DeInit，TIM，DeInit，
+    // ，TIM
+//    __HAL_RCC_TIM_CLK_DISABLE();
+//    HAL_NVIC_DisableIRQ(TIM_IRQn);
+}
